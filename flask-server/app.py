9b43ce49376160b2ae9fad108cead0a8fa16f509
ekko-technology <joshyeo798@gmail.com>
2025-07-08 00:58:53 +0800

    Fix: create tables on first request for Render

--- flask-server/app.py
+++ flask-server/app.py
@@ -66,6 +66,10 @@ class GameRecord(db.Model):
     played_at = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
 
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 
 @app.route('/')
 def index():
